ec50f53cd0c8d7cea7afc498ac55364494d6d0e1
unknown <aguirreh8@gmail.com>
2017-12-25 23:09:34 -0500

    created wrestlerRoster array, createWrestlerBtn function and #submitRequest button property

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,34 @@
+//construct list of premade wrestlers
+const wrestlerList = [
+	"John Cena",
+	"Stone Cold Steve Austin",
+	"The Ultimate Warrior",
+	"Razor Ramon",
+	"Bret Hart",
+	"The Undertaker",
+	"Kane",
+	"Rey Mysterio",
+	"Eddie Guerrero"
+];
+
+//creates a new button for each item in wrestlerList array
+function createWrestlerBtn() {
+	$("#wrestlerSelect").empty();
+	for (let i = 0; i < wrestlerList.length; i++) {
+		const newBtn = $("<span class='inputButton wrestlerBtn'>");
+		newBtn.attr("data-name", wrestlerList[i]);
+		newBtn.text(wrestlerList[i]);
+		$("#wrestlerSelect").append(newBtn);
+	}
+}
+
+
 $(document).ready(function() {
-	$(".wrestlerBtn").click(function() {
-		alert("hello!");
+	createWrestlerBtn();
+	//clicking submit button will append a new item to wrestlerList and remake the button list
+	$("#submitRequest").click(function() {
+		const newWrestler = $("#newWrestler").val();
+		wrestlerList.push(newWrestler);
+		createWrestlerBtn();
 	})
 })
