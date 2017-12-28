fbfa5be0766edf9bc22f38f53c7d56851127d3c7
unknown <aguirreh8@gmail.com>
2017-12-27 20:37:28 -0500

    added click function to gif to switch from still to animating

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ const wrestlerRoster = [
 	"John Cena",
 	"Stone Cold Steve Austin",
 	"The Ultimate Warrior",
-	"Razor Ramon",
+	"Scott Hall",
 	"Bret Hart",
 	"The Undertaker",
 	"Kane",
@@ -13,6 +13,7 @@ const wrestlerRoster = [
 
 //displays 10 gifs based on the wrestler name given
 function getGifs(clicked) {
+	$("#gifDisplay").empty();
 	const search = clicked;
 	const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qEsTCW0qE324onxIHrwwXq5Cr0DU8ccP&q=" + search + "&limit=10";
 	$.ajax({
@@ -22,12 +23,13 @@ function getGifs(clicked) {
 		for (let i = 0; i < response.data.length; i++) {
 			const newDiv = $("<div class='gifHolder'>");
 			const rating = $("<p>");
-			const newImg = $("<img>");
+			const newImg = $("<img data-state='still' class='wrestlerGif'>");
 			rating.text("Rating: " + response.data[i].rating);
-			newImg.attr("src", response.data[i].images.original.url);
+			newImg.attr("src", response.data[i].images.fixed_height_still.url);
+			newImg.attr("data-still", response.data[i].images.fixed_height_still.url)
+			newImg.attr("data-animate", response.data[i].images.fixed_height.url)
 			newDiv.append(rating, newImg);
 			$("#gifDisplay").append(newDiv);
-			console.log(response); 
 		}
 	})
 }
@@ -51,5 +53,25 @@ $(document).ready(function() {
 		const newWrestler = $("#newWrestler").val();
 		wrestlerRoster.push(newWrestler);
 		createWrestlerBtn();
+	})
+
+	$(document).on("click", ".wrestlerBtn", function() {
+		const value = $(this).data("name");
+		getGifs(value);
+	})
+
+	$(document).on("click", ".wrestlerGif", function() {
+		const animate = $(this).attr("data-animate");
+		const still = $(this).attr("data-still");
+		const state = $(this).attr("data-state");
+
+		if (state === "still") {
+			$(this).attr("src", animate);
+			$(this).attr("data-state", "animating");
+		} else {
+			$(this).attr("src", still);
+			$(this).attr("data-state", "still");
+		}
+
 	})
 })
